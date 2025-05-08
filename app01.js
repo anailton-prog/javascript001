bcff5d3f7c21d61a202f6f89d8570a949dd018e3
anailton <jesusanailton706@gmail.com>
2025-05-08 20:17:28 -0300

    operadores

--- app01.js
+++ app01.js
@@ -6,7 +6,7 @@ let nome = "Anailton"
 let idade = 19
 
 // // Console e Debug
-console.log(nome) 
+console.log(nome)
 console.log(idade)
 nome = "alberto"
 idade = "60"
@@ -25,17 +25,42 @@ let fumante = false
 console.log(typeof cidade)
 console.log(typeof salario)
 console.log(typeof fumante)
-// // Operadores
-// //     Operador Atribuição
-// //     Operadores aritméticos
-// //     Operadores relacionais
-// //     Operadores lógicos
 
+// // Operadores
+// //     Operador Atribuição ( = )
+// //     Operadores aritméticos (+, -, /, *, % )
+
+console.log(10 + 15)
+let n1 = 10
+let n2 = 5
+console.log(n1 + n2) // soma
+console.log(n1 - n2) // subtração
+console.log(n1 / n2) // divisão
+console.log(n1 * n2) // multiplicação
+console.log(n1 % n2) // módulo (resto da divisão)
+
+// //     Operadores relacionais ( >, <, >=, <=, !=, == )
+
+console.log(n1 == n2)
+console.log(n1 != n2)
+console.log(n1 > n2)
+console.log(n1 < n2)
+console.log(n1 <= n2)
+console.log(n1 >= n2)
+
+// // operadores lógicos (!, &&, || )
+
+console.log(!10 > 2) // não - inverte o resultado, se for verdadeiro muda pra falso, se for falso muda pra verdadeiro
+console.log(!false)
+
+console.log(10 > 2 && 35 < 100 && n1 > n2 && 100 < 90) // E - todas as verificações precisam ser verdadeiras para o resultado ser verdadeiro
+
+console.log(10<2 || 100==150 || 57==57) // ou - apenas uma verificação, precisa ser verdadeira para o resultado ser verdadeiro, o resultado só será falso quando TODAS as verificações forem falsas
 // // Estrutura
 // //     Estrutura de controle/decisão
 // //     Laços de repetição
 
-// // Arrays 
+// // Arrays
 
 // // Funções básicas
 // // Escopo
@@ -56,4 +81,3 @@ console.log(typeof fumante)
 
 
 // // Spread e REST  ************
- 
